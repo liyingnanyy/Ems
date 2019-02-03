3511e815b95ae78939ccfdb9ada88eace9233724
liyingnanyy <1532389462@qq.com>
2019-02-03 11:22:38 +0800

    Login

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-link to="/login"></router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
